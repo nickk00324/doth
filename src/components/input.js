bef7ea84efa6a31e75c6d141bfe4c223d3d0b801
nickk <nickkochornswasdi@gmail.com>
2020-01-08 23:22:28 -0800

    added key press functionality to commit line

--- src/components/input.js
+++ src/components/input.js
@@ -9,13 +9,18 @@ import {
 
 const Input = props => {
 
+    const handleEnter = e => {
+        if(e.charCode === 13) props.doneEditing(ADD_LINE);
+    }
+
     return(
-        <div className="input" style={{
+        <div className="input" onKeyPress={handleEnter}
+            style={{
             position: 'absolute',
             top: `${props.editLocation.y}px`,
             left: `${props.editLocation.x}px`
         }}>
-            <textarea value={props.editValue} className="input-text" onChange={props.handleInput}></textarea>
+            <textarea autoFocus value={props.editValue} className="input-text" onChange={props.handleInput}></textarea>
             <button className="input-button add-button" onClick={() => props.doneEditing(ADD_LINE)}>add</button>
             <button className="input-button exit-button" onClick={() => props.doneEditing(EXIT)}>exit</button>
             <button className="input-button remove-button" onClick={() => props.doneEditing(REMOVE_LINE)}>remove</button>
